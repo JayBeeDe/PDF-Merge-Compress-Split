fe5020c4e976654f2020365cf609497a3c4f7cea
JayBeeDe <jb.social@outlook.com>
2016-12-24 00:47:52 +0100

    Adding feature and improvements
    translation improvements
    CLI support
    merge feature add

--- Main.ps1
+++ Main.ps1
@@ -1,140 +1,78 @@
-﻿param (
+﻿<#
+Application: PDF Merge Compress Split
+Description: This PowerShell Script can clean your start menu and your system shell.
+Author: Jean-Baptiste Delon
+Third Dependency: gswin64c.exe Ghostscript software
+
+License: the script (wihout its tird dependency) is under GNU GPL v3.0 license and can be edited, distributed for commercial/private use.
+For the Ghostscript software license, see http://www.artifex.com/page/licensing-information.html.
+#>
+
+param (
    [string]$inputDirectory,
+   [string]$mode,
    [boolean]$cli,
+   [boolean]$translate,
+   [boolean]$recurse,
+   [boolean]$autoRotate,
    [string]$outName
 )
-
-
-function actionFileBrowse($path){
-    $path=getRootPath $path
-    $fileBox=New-Object System.Windows.Forms.OpenFileDialog
-    $fileBox.InitialDirectory=$path
-    $fileBox.Filter="pdf files (*.pdf)|*.pdf"
-    $fileBox.Multiselect=$true
-    $fileBox.Title="Select the file"
-    $fileBox.CheckFileExists=$true
-    
-    $res=$fileBox.ShowDialog()
-    if($res -eq "OK"){
-        $global:textBox.Text=$fileBox.FileName
-    }
-}
-function actionFolderBrowse($path){
-    $path=getRootPath $path
-    $folderBox=New-Object System.Windows.Forms.FolderBrowserDialog
-    $folderBox.RootFolder="MyComputer"
-    $folderBox.SelectedPath=$path
-    $folderBox.ShowNewFolderButton=$true
-    $folderBox.Description="Select the directory the script will search for pdf files"
-    
-    $res=$folderBox.ShowDialog()
-    if($res -eq "OK"){
-        $global:textBox.Text=$folderBox.SelectedPath
-    }
-}
-function actionFileProcess($path){
-    $global:progressStatus.Visible=$false
-    $global:errorLabel.Visible=$false
-    $global:successLabel.Visible=$false
-
-    if(Test-Path -Path $path -PathType Any){
-        if(Test-Path -Path $path -PathType Leaf){
-            $listInput=@($path)
-        }else{
-            try{
-                if($global:recurseCheckbox.Checked -eq $true){
-                    $listInput=@((Get-ChildItem -Path $path -Recurse | Where-Object{$_.PSIsContainer -ne $true -and $_.Extension -eq ".pdf"} -ErrorAction Stop).FullName)
-                }else{
-                    $listInput=@((Get-ChildItem -Path $path | Where-Object{$_.PSIsContainer -ne $true -and $_.Extension -eq ".pdf"} -ErrorAction Stop).FullName)
-                }
-            }catch{
-                $global:errorLabel.Text="Unable to list the item of the requesyed path!"
-                $global:errorLabel.Visible=$true
-                $global:form.Refresh()
-                return
-            }
-        }
-    }
-    
-    if($listInput -eq $null){
-        $global:progressStatus.Visible=$false
-        $global:errorLabel.Text="The requested path doesn't contain any pdf files!"
-        $global:errorLabel.Visible=$true
-        $global:errorLabel
-        $global:form.Refresh()
-        return
-    }
-
-    if(!(Test-Path -Path "$($global:currentLocation)\gswin64c.exe" -PathType Leaf)){
-        $global:progressStatus.Visible=$false
-        $global:errorLabel.Text="The gswin64c.exe executable is not present in $($global:currentLocation)!"
-        $global:errorLabel.Visible=$true
-        $global:errorLabel
-        $global:form.Refresh()
-        return
-    }
-
-    $global:progressBar.Value=0
-    $global:progressBar.Visible=$true
-    $global:progressStatus.Visible=$true
-    $global:progressBar.Maximum=$listInput.Length
-    $global:form.Refresh()
-
-    for($i=0;$i -lt $listInput.Length;$i++){
-        $global:progressStatus.Text="Processing $($listInput[$i].FullName)..."
-        $global:form.Refresh()
-
-        $res=&"$($global:currentLocation)\gswin64c.exe" "-sDEVICE=pdfwrite" "-dCompatibilityLevel=1.4" "-dPDFSETTINGS=/ebook" "-dNOPAUSE" "-dQUIET" "-dBATCH" "-sOutputFile=$($global:currentLocation)\$($global:outName)$($i).pdf" "$($listInput[$i].FullName)"
-            
-        if($res -ne "" -and $res -ne $null){
-            $global:progressStatus.Visible=$false
-            $global:errorLabel.Text="Error while processing $($listInput[$i].FullName): $($res)"
-            $global:errorLabel.Visible=$true
-            $global:errorLabel
-            $global:form.Refresh()
-            return
-        }
-
-        $global:progressBar.Value=$i+1
-        $global:form.Refresh()
-    }
-    $global:progressStatus.Visible=$false
-    $global:successLabel.Visible=$true
-    $global:form.Refresh()
-}
-
-function getRootPath($path){
-    if(Test-Path -Path $path -PathType Any){
-        if(Test-Path -Path $path -PathType Leaf){
-            try{
-                $newPath=Split-Path -Path $path -ErrorAction Stop
-            }catch{
-                return $global:currentLocation
-            }
-            return $newPath
-        }else{
-            return $path
-        }
-    }else{
-        return $global:currentLocation
-    }
-}
 
 $global:currentLocation=Split-Path -Path $MyInvocation.MyCommand.Path
 $global:applicationName="PDF Merge Compress Split"
 $global:defaultOutName="output"
+$global:TranslateAccountKey="a9405b496e35440882154d696d71140c"
+$global:TranslateTokenURL="https://api.cognitive.microsoft.com/sts/v1.0/issueToken"
+$global:TranslateURL="https://api.microsofttranslator.com/v2/Http.svc/Translate"
 
-if($outName -eq $null -or $outName -eq ""){
-    $global:outName=$global:defaultOutName
+try{
+    Import-Module "$($global:currentLocation)\Core.psm1" -Force -ErrorAction Stop -Scope Local
+}catch{
+    write-host "An error has occured while loading the function core module" -ForegroundColor Red
+    Exit   
+}
+try{
+    $global:systemLanguage=(Get-Culture).TwoLetterISOLanguageName
+}catch{
+    $global:systemLanguage="en"
+    write-host "Unable to detect system language - The script has been set to the default system language - English!" -ForegroundColor Yellow
+}
+
+cd /
+cls
+display "The script is begining!"
+
+if($mode -eq "Merge"){
+    $global:mode="m"
 }else{
-    $global:outName=$outName
+    $global:mode="c"
 }
 
 if($cli -eq $true){
     $global:cli=$true
 }else{
     $global:cli=$false
-}#todo
+}
+if($translate -eq $false){
+    $global:translate=$false
+}else{
+    $global:translate=$true
+}
+if($recurse -eq $true){
+    $global:recurse=$true
+}else{
+    $global:recurse=$false
+}
+if($autoRotate -eq $false){
+    $global:autoRotate=$false
+}else{
+    $global:autoRotate=$true
+}
+if($outName -eq $null -or $outName -eq ""){
+    $global:outName=$global:defaultOutName
+}else{
+    $global:outName=$outName
+}
 
 try{
     if(Test-Path -Path $inputDirectory -PathType Any -ErrorAction Stop){
@@ -148,108 +86,142 @@ try{
 
 if($global:cli -eq $true){
     actionFileProcess($global:inputDirectory)
-    exit
+}else{
+    # Chargement des assemblies
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
+    # Creation de la form principale
+    $global:form=New-Object Windows.Forms.Form
+    $global:form.FormBorderStyle=[System.Windows.Forms.FormBorderStyle]::FixedDialog
+    $global:form.MaximizeBox=$False
+    $global:form.MinimizeBox=$True
+    $global:form.Text=$global:applicationName
+    $global:form.Size=New-Object System.Drawing.Size(400,370)
+    $global:form.Icon=New-Object system.drawing.icon("$($global:currentLocation)\pdf.ico")
+    $global:form.StartPosition="CenterScreen"
+
+    $label1=New-Object System.Windows.Forms.Label
+    $label1.Text=translate "Select the file or folder you want to process pdf files"
+    $label1.Font="Arial,10"
+    $label1.Location=New-Object System.Drawing.Point(5,5)
+    $label1.MaximumSize=New-Object System.Drawing.Size(380,44)
+    $label1.AutoSize=$true
+    $global:form.Controls.Add($label1)
+
+    $global:textBox=New-Object System.Windows.Forms.TextBox
+    $global:textBox.Location=New-Object System.Drawing.Point(5,45)
+    $global:textBox.Size=New-Object System.Drawing.Size(380,22)
+    $global:textBox.AllowDrop=$true
+    $global:textBox.Text=$global:inputDirectory
+    $global:form.Controls.Add($global:textBox)
+
+    $fileButton=New-Object System.Windows.Forms.Button
+    $fileButton.Location=New-Object System.Drawing.Point(25,70)
+    $fileButton.Size=New-Object System.Drawing.Size(150,22)
+    $fileButton.Text=translate "Browse File"
+    $fileButton.Add_Click({actionFileBrowse $global:textBox.Text})
+    $global:form.Controls.Add($fileButton)
+
+    $folderButton=New-Object System.Windows.Forms.Button
+    $folderButton.Location=New-Object System.Drawing.Point(215,70)
+    $folderButton.Size=New-Object System.Drawing.Size(150,22)
+    $folderButton.Text=translate "Browse Folder"
+    $folderButton.Add_Click({actionFolderBrowse $global:textBox.Text})
+    $global:form.Controls.Add($folderButton)
+    
+    $global:recurseCheckbox=New-Object System.Windows.Forms.CheckBox
+    $global:recurseCheckbox.Location=New-Object System.Drawing.Point(5,110)
+    $global:recurseCheckbox.MaximumSize=New-Object System.Drawing.Size(200,22)
+    $global:recurseCheckbox.Text=translate "Browse recurse folder"
+    if($global:recurse -eq $true){
+        $global:recurseCheckbox.Checked=$true
+    }
+    $global:recurseCheckbox.AutoSize=$true
+    $global:form.Controls.Add($global:recurseCheckbox)
+    
+    $global:autoRotateCheckbox=New-Object System.Windows.Forms.CheckBox
+    $global:autoRotateCheckbox.Location=New-Object System.Drawing.Point(205,110)
+    $global:autoRotateCheckbox.MaximumSize=New-Object System.Drawing.Size(200,22)
+    $global:autoRotateCheckbox.Text=translate "Automatic rotation of the page"
+    if($global:autoRotate -eq $true){
+        $global:autoRotateCheckbox.Checked=$true
+    }
+    $global:autoRotateCheckbox.AutoSize=$true
+    $global:form.Controls.Add($global:autoRotateCheckbox)
+    
+    $global:modeRadio1=New-Object System.Windows.Forms.RadioButton
+    $global:modeRadio1.Location=New-Object System.Drawing.Point(60,140)
+    $global:modeRadio1.MaximumSize=New-Object System.Drawing.Size(250,22)
+    $global:modeRadio1.Text=translate "Compress pdf files individually"
+    if($global:mode -eq "c"){
+        $global:modeRadio1.Checked=$true
+    }
+    $global:modeRadio1.AutoSize=$true
+    $global:form.Controls.Add($global:modeRadio1)
+    
+    $global:modeRadio2=New-Object System.Windows.Forms.RadioButton
+    $global:modeRadio2.Location=New-Object System.Drawing.Point(60,170)
+    $global:modeRadio2.MaximumSize=New-Object System.Drawing.Size(250,22)
+    $global:modeRadio2.Text=translate "Merge and Compress pdf files"
+    if($global:mode -eq "m"){
+        $global:modeRadio2.Checked=$true
+    }
+    $global:modeRadio2.AutoSize=$true
+    $global:form.Controls.Add($global:modeRadio2)
+
+    $global:successLabel=New-Object System.Windows.Forms.Label
+    $global:successLabel.Visible=$false
+    $global:successLabel.Font="Arial,8"
+    $global:successLabel.ForeColor="Green"
+    $global:successLabel.MaximumSize=New-Object System.Drawing.Size(380,66)
+    $global:successLabel.Location=New-Object System.Drawing.Point(5,200)
+    $global:successLabel.AutoSize=$true
+    $global:form.Controls.Add($global:successLabel)
+
+    $global:errorLabel=New-Object System.Windows.Forms.Label
+    $global:errorLabel.Visible=$false
+    $global:errorLabel.Font="Arial,8"
+    $global:errorLabel.ForeColor="Red"
+    $global:errorLabel.MaximumSize=New-Object System.Drawing.Size(380,66)
+    $global:errorLabel.Location=New-Object System.Drawing.Point(5,200)
+    $global:errorLabel.AutoSize=$true
+    $global:form.Controls.Add($global:errorLabel)
+
+    $global:progressStatus=New-Object System.Windows.Forms.Label
+    $global:progressStatus.Visible=$false
+    $global:progressStatus.Font="Arial,8"
+    $global:progressStatus.MaximumSize=New-Object System.Drawing.Size(380,66)
+    $global:progressStatus.Location=New-Object System.Drawing.Point(5,200)
+    $global:progressStatus.AutoSize=$true
+    $global:form.Controls.Add($global:progressStatus)
+
+    $global:progressBar=New-Object System.Windows.Forms.ProgressBar
+    $global:progressBar.Location=New-Object System.Drawing.Point(5,250)
+    $global:progressBar.Size=New-Object System.Drawing.Size(380,22)
+    $global:progressBar.Visible=$true
+    $global:progressBar.Minimum=0
+    $global:progressBar.Step=1
+    $global:progressBar.Style="Continuous"
+    $global:progressBar.Visible=$false
+    $global:form.Controls.Add($global:progressBar)
+
+    $CancelButton=New-Object System.Windows.Forms.Button
+    $CancelButton.Location=New-Object System.Drawing.Point(50,300)
+    $CancelButton.Size=New-Object System.Drawing.Size(75,23)
+    $CancelButton.Text=translate "Cancel"
+    $CancelButton.DialogResult=[System.Windows.Forms.DialogResult]::Cancel
+    $global:form.CancelButton=$CancelButton
+    $global:form.Controls.Add($CancelButton)
+
+    $OKButton=New-Object System.Windows.Forms.Button
+    $OKButton.Location=New-Object System.Drawing.Point(240,300)
+    $OKButton.Size=New-Object System.Drawing.Size(75,23)
+    $OKButton.Text=translate "Start"
+    $OKButton.Add_Click({actionFileProcess $global:textBox.Text})
+    $global:form.Controls.Add($OKButton)
+
+    $global:form.ShowDialog()
 }
 
-# Chargement des assemblies
-[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-[void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-
-# Creation de la form principale
-$global:form=New-Object Windows.Forms.Form
-$global:form.FormBorderStyle=[System.Windows.Forms.FormBorderStyle]::FixedDialog
-$global:form.MaximizeBox=$False
-$global:form.MinimizeBox=$True
-$global:form.Text=$global:applicationName
-$global:form.Size=New-Object System.Drawing.Size(400,370)
-$global:form.Icon=New-Object system.drawing.icon("$($global:currentLocation)\pdf.ico")
-$global:form.StartPosition = "CenterScreen"
-
-$label1=New-Object System.Windows.Forms.Label
-$label1.Text="Select the file or folder you want to process pdf files"
-$label1.Font="Arial,12"
-$label1.Location=New-Object System.Drawing.Point(5,5)
-$label1.Size=New-Object System.Drawing.Size(0,22)
-$label1.AutoSize=$true
-$global:form.Controls.Add($label1)
-
-$global:textBox=New-Object System.Windows.Forms.TextBox
-$global:textBox.Location=New-Object System.Drawing.Point(5,35)
-$global:textBox.Size=New-Object System.Drawing.Size(380,22)
-$global:textBox.AllowDrop=$true
-$global:textBox.Text=$global:inputDirectory
-$global:form.Controls.Add($global:textBox)
-
-$fileButton=New-Object System.Windows.Forms.Button
-$fileButton.Location=New-Object System.Drawing.Point(50,70)
-$fileButton.Size=New-Object System.Drawing.Size(100,22)
-$fileButton.Text="Browse File"
-$fileButton.Add_Click({actionFileBrowse $global:textBox.Text})
-$global:form.Controls.Add($fileButton)
-
-$folderButton=New-Object System.Windows.Forms.Button
-$folderButton.Location=New-Object System.Drawing.Point(240,70)
-$folderButton.Size=New-Object System.Drawing.Size(100,22)
-$folderButton.Text="Browse Folder"
-$folderButton.Add_Click({actionFolderBrowse $global:textBox.Text})
-$global:form.Controls.Add($folderButton)
-
-$global:recurseCheckbox=New-Object System.Windows.Forms.CheckBox
-$global:recurseCheckbox.Location=New-Object System.Drawing.Point(5,110)
-$global:recurseCheckbox.Size=New-Object System.Drawing.Size(200,22)
-$global:recurseCheckbox.Text="Browse recurse folder"
-$global:form.Controls.Add($global:recurseCheckbox)
-
-$global:successLabel=New-Object System.Windows.Forms.Label
-$global:successLabel.Visible=$false
-$global:successLabel.Text="The requested pdf files have been successfully processed!"
-$global:successLabel.Font="Arial,8"
-$global:successLabel.ForeColor="Green"
-$global:successLabel.MaximumSize=New-Object System.Drawing.Size(380,66)
-$global:successLabel.Location=New-Object System.Drawing.Point(5,200)
-$global:successLabel.AutoSize=$true
-$global:form.Controls.Add($global:successLabel)
-
-$global:errorLabel=New-Object System.Windows.Forms.Label
-$global:errorLabel.Visible=$false
-$global:errorLabel.Font="Arial,8"
-$global:errorLabel.ForeColor="Red"
-$global:errorLabel.MaximumSize=New-Object System.Drawing.Size(380,66)
-$global:errorLabel.Location=New-Object System.Drawing.Point(5,200)
-$global:errorLabel.AutoSize=$true
-$global:form.Controls.Add($global:errorLabel)
-
-$global:progressStatus=New-Object System.Windows.Forms.Label
-$global:progressStatus.Visible=$false
-$global:progressStatus.Font="Arial,8"
-$global:progressStatus.MaximumSize=New-Object System.Drawing.Size(380,66)
-$global:progressStatus.Location=New-Object System.Drawing.Point(5,200)
-$global:progressStatus.AutoSize=$true
-$global:form.Controls.Add($global:progressStatus)
-
-$global:progressBar=New-Object System.Windows.Forms.ProgressBar
-$global:progressBar.Location=New-Object System.Drawing.Point(5,250)
-$global:progressBar.Size=New-Object System.Drawing.Size(380,22)
-$global:progressBar.Visible=$true
-$global:progressBar.Minimum=0
-$global:progressBar.Step=1
-$global:progressBar.Style="Continuous"
-$global:progressBar.Visible=$false
-$global:form.Controls.Add($global:progressBar)
-
-$CancelButton=New-Object System.Windows.Forms.Button
-$CancelButton.Location=New-Object System.Drawing.Point(50,300)
-$CancelButton.Size=New-Object System.Drawing.Size(75,23)
-$CancelButton.Text="Cancel"
-$CancelButton.DialogResult=[System.Windows.Forms.DialogResult]::Cancel
-$global:form.CancelButton=$CancelButton
-$global:form.Controls.Add($CancelButton)
-
-$OKButton=New-Object System.Windows.Forms.Button
-$OKButton.Location=New-Object System.Drawing.Point(240,300)
-$OKButton.Size=New-Object System.Drawing.Size(75,23)
-$OKButton.Text="Start"
-$OKButton.Add_Click({actionFileProcess $global:textBox.Text})
-$global:form.Controls.Add($OKButton)
-
-$global:form.ShowDialog()
+display "The script has finished!"
--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## GOAL
 
 The script aims at performing the following tasks on pdf files:
-- Only individually compress some pdf files (Compress Only)
+- Only individually compress some pdf files (Compress Only) | Will replace the original file(s)!
 - Merge some pdf files into one and compress the output file (Merge and Compress)
-- Merge some pdf files into one, compress and split into several output files (Merge, Compress and Split)
+- Merge some pdf files into one, compress and split into several output files (Merge, Compress and Split) (NOT IMPLEMETED YET)
 
 This powershell is a simple interface (GUI or CLI) that calls the Ghostscript software (gswin64c.exe)
 
@@ -36,8 +36,27 @@ information.
 
 Download and unzip (if needed) the files.
 
-The script can be run directly by double clicking the script, or in command line. From command line, it
-accepts arguments (not finished yet)
+The script can be run directly by double clicking the script, or in command line.
+
+Note: Execution Policy must be ENABLED in order to get the PowerShell script running.
+Please See https://msdn.microsoft.com/en-us/powershell/reference/5.1/microsoft.powershell.security/set-executionpolicy
+
+From command line, it accepts the following arguments:
+-inputDirectory <string>: targeted directory or file (default: the current directory)
+-mode <string>: [C/m] - compress / merge and compress
+-cli <boolean>: [$true/$FALSE] - enable/disable CLI instead of GUI
+-translate <boolean>: [$TRUE/$false] - enable/disable translation from english to the system menu (internet connexion required)
+-recurse <boolean>: [$true/$FALSE] - If inputDirectory is a directory, wether search for file also recursively
+-autoRotate <boolean>: [$TRUE/$false] - try to auto rotate pages
+-outName <string>: output file when merging files (default value defined by $global:defaultOutName)
+
+## EXAMPLE
+
+In GUI: just double click the file or run from a PowerShell terminal:
+.\Main.ps1
+
+In CLI, to compress each pdf file in the script's directory (script referential is from the Main.ps1 file), run:
+.\Main.ps1 -cli $true
 
 This script doesn't need to be run as Administrator
 > 23-12-2016 | Jean-Baptiste DELON
